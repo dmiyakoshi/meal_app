a044137d8640de00e779783cd9cc41e7230f2e97
dmiyakoshi <d1m354dai@gmail.com>
2021-10-12 19:15:40 +0900

    経過日時を表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,14 @@
             @foreach ($posts as $post)
                 <article class="w-full px-4 md:w-1/2 text-xl text-gray-800 leading-normal">
                     <a href="{{ route('posts.show', $post) }}">
-                        <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl">{{ $post->title }}</h2>
+                        <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl">
+                            {{ $post->title }}</h2>
                         <h3>{{ $post->user->name }}</h3>
-                        <p class="text-sm mb-2 md:text-base font-normal text-gray-600">
-                            <span class="text-red-400 font-bold">{{ date('Y-m-d H:i:s', strtotime('-1 day')) < $post->created_at ? 'NEW' : '' }}</span>
-                            {{ $post->created_at }}
+                        <p class="text-sm  md:text-base font-normal text-gray-600">
+                            <span>経過日時: {{ $post->created_at->diffForHumans() }}</span>
+                        </p>
+                        <p class="text-sm md:text-base font-normal text-gray-600">
+                            記事作成日: {{ $post->created_at }}
                         </p>
                         <img class="w-full mb-2" src="{{ $post->image_url }}" alt="image">
                         <p class="text-gray-700 text-base">{{ Str::limit($post->body, 10) }}</p>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,9 @@
 <script>
-    function DisableButton(btn){
-    btn.disabled=true;
-    btn.value='送信中...';
-    btn.form.submit();
-}
+    function DisableButton(btn) {
+        btn.disabled = true;
+        btn.value = '送信中...';
+        btn.form.submit();
+    }
 </script>
 
 <x-app-layout>
@@ -14,29 +14,33 @@
             <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl">
                 {{ $post->title }}</h2>
             <h3>{{ $post->user->name }}</h3>
-            <p class="text-sm mb-2 md:text-base font-normal text-gray-600">
-                <span
-                    class="text-red-400 font-bold">{{ date('Y-m-d H:i:s', strtotime('-1 day')) < $post->created_at ? 'NEW' : '' }}</span>
-                {{ $post->created_at }}
+            <p class="text-sm  md:text-base font-normal text-gray-600">
+                <span>経過日時: {{ $post->created_at->diffForHumans() }}</span>
+            </p>
+            <p class="text-sm md:text-base font-normal text-gray-600">
+                記事作成日: {{ $post->created_at }}
             </p>
             <img src="{{ Storage::url($post->image) }}" alt="image" class="mb-4">
             <p class="text-gray-700 text-base mb-3">{!! nl2br(e($post->body)) !!}</p>
             @auth
-            @if ($like)
-                <a href="{{ route('unlike', $post) }}" onclick="if(!confirm('お気に入りを解除しますか？')){return false};" class="bg-pink-400 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2 mt-2 mb-2">
-                    お気に入り削除
-                </a>
-            @else
-                <a href="{{ route('like', $post) }}" onclick="if(!confirm('お気に入りにしますか？')){return false};" class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2 mt-2 mb-2">
-                    お気に入り
-                </a>
-            @endif
-            <p class="mt-3">お気に入り数: {{ $post->likes->count() }}</p>
+                @if ($like)
+                    <a href="{{ route('unlike', $post) }}" onclick="if(!confirm('お気に入りを解除しますか？')){return false};"
+                        class="bg-pink-400 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2 mt-2 mb-2">
+                        お気に入り削除
+                    </a>
+                @else
+                    <a href="{{ route('like', $post) }}" onclick="if(!confirm('お気に入りにしますか？')){return false};"
+                        class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2 mt-2 mb-2">
+                        お気に入り
+                    </a>
+                @endif
+                <p class="mt-3">お気に入り数: {{ $post->likes->count() }}</p>
             @endauth
         </article>
         <div class="flex flex-row text-center my-4">
             @can('update', $post)
-                <a href="{{ route('posts.edit', $post) }}" class="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 h-10 mr-2">編集</a>
+                <a href="{{ route('posts.edit', $post) }}"
+                    class="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 h-10 mr-2">編集</a>
             @endcan
             @can('delete', $post)
                 <form action="{{ route('posts.destroy', $post) }}" method="post">
